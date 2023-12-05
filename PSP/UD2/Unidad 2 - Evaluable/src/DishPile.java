1cb23ce5eaacf320977f478f686843f6a47c1f96
Ronin <codigoronin@gmail.com>
2023-12-05 09:50:58 +0100

    DishPile with array

--- PSP/UD2/Unidad 2 - Evaluable/src/DishPile.java
+++ PSP/UD2/Unidad 2 - Evaluable/src/DishPile.java
@@ -1,39 +1,43 @@
 
-import java.util.Stack;
-
 public class DishPile 
 {
     // Maximum number of dishes allowed in the pile
+   
     public static final int MAX_DISHES = 5;
-    
-    Stack<Dish> pile;
-    
-    public DishPile()
-    {
-        pile = new Stack<Dish>();
+    private Dish[] dishArray;
+    private int count;
+
+    public DishPile() {
+        dishArray = new Dish[MAX_DISHES];
+        count = 0;
     }
-    
-    public synchronized void wash(Dish d)
-    {
-        if (pile.size() == MAX_DISHES)
-            try
-            {
+
+    public synchronized void wash(Dish d) {
+        while (count == MAX_DISHES) {
+            try {
                 wait();
-            } catch (Exception e) {}
-        pile.push(d);
-        System.out.println("Washed dish #" + d.getNumber() + ", total in pile: " + pile.size());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        dishArray[count] = d;
+        count++;
+        System.out.println("Washed dish #" + d.getNumber() + ", total in array: " + count);
         notifyAll();
     }
-    
-    public synchronized void dry()
-    {
-        if (pile.size() == 0)
-            try
-            {
+
+    public synchronized void dry() {
+        while (count == 0) {
+            try {
                 wait();
-            } catch (Exception e) {}
-        Dish d = pile.pop();
-        System.out.println("Drying dish #" + d.getNumber() + ", total in pile: " + pile.size());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Dish d = dishArray[count - 1];
+        count--;
+        dishArray[count] = null; // Remove the reference to the dried dish
+        System.out.println("Drying dish #" + d.getNumber() + ", total in array: " + count);
         notifyAll();
     }
 }
---
+++ PSP/UD2/Unidad 2 - Evaluable/src/DishPileStack.java
@@ -0,0 +1,39 @@
+
+import java.util.Stack;
+
+public class DishPileStack
+{
+    // Maximum number of dishes allowed in the pile
+    public static final int MAX_DISHES = 5;
+    
+    Stack<Dish> pile;
+    
+    public DishPileStack()
+    {
+        pile = new Stack<Dish>();
+    }
+    
+    public synchronized void wash(Dish d)
+    {
+        if (pile.size() == MAX_DISHES)
+            try
+            {
+                wait();
+            } catch (Exception e) {}
+        pile.push(d);
+        System.out.println("Washed dish #" + d.getNumber() + ", total in pile: " + pile.size());
+        notifyAll();
+    }
+    
+    public synchronized void dry()
+    {
+        if (pile.size() == 0)
+            try
+            {
+                wait();
+            } catch (Exception e) {}
+        Dish d = pile.pop();
+        System.out.println("Drying dish #" + d.getNumber() + ", total in pile: " + pile.size());
+        notifyAll();
+    }
+}
